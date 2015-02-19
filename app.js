8e6d17958adf5e85d03b41e5aa483ad7f76811bd
Bailey Busch <baileybusch@gmail.com>
2015-02-19 01:08:32 -0800

    server functionality

--- app.js
+++ app.js
@@ -1,24 +1,66 @@
-var fs = require('fs');
-var https = require('https');
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
+// variable declarations
+var fs         = require('fs'),
+	http       = require('http'),
+	express    = require('express'),
+	bodyParser = require('body-parser'),
+	request    = require('request'),
+	app        = express(),
+	server     = http.createServer(app),
+	// Set your secret key: remember to change this to your live secret key in production
+	// See your keys here https://dashboard.stripe.com/account
+	stripe     = require("stripe")("sk_test_cTcGfvRhAUPZ4wZvHoxSKjU0");
 
-// Set your secret key: remember to change this to your live secret key in production
-// See your keys here https://dashboard.stripe.com/account
-var stripe = require("stripe")("pk_test_5jkypj4J9HFYKz9TWiPEm2Y5");
+// for one time payments:
+function chargeCard(token, res){
+	stripe.charges.create({
+		amount: 1000, // amount to charge in cents
+		currency: 'usd',
+		card: token, //the token from the client
+		description: 'Test charge from ' + (new Date()),
+	}, function(err, charge) {
+		if (err) {
+			res.send({ok: false, message: 'There was a problem processing your card (error: ' + JSON.stringify(err) + ')'});
+		} else {
+			res.send({ok: true, message: 'Your transaction completed!'})
+		}
+	});
+} 
 
-// (Assuming you're using express - expressjs.com)
-// Get the credit card details submitted by the form
-var stripeToken = request.body.stripeToken;
+// server listen to port 8000 and run express configuration
+app.listen(8000);
+console.log("server running on localhost:8000");
+app.use(bodyParser.json()); // lets us parse JSON from POST requests
+app.disable('x-powered-by'); // this stops Express from broadcasting that you're using Express, which is always good practice
+app.use(bodyParser.urlencoded({ extended: false }))
 
-var charge = stripe.charges.create({
-  amount: 1000, // amount in cents, again
-  currency: "usd",
-  card: stripeToken,
-  description: "payinguser@example.com"
-}, function(err, charge) {
-  if (err && err.type === 'StripeCardError') {
-    // The card has been declined
-  }
+
+// server handle first client request
+app.get('/', function (request, response) {
+	response.sendFile(__dirname + '/index.html'); //send the payment form file
 });
+
+// server handle request to '/charge' 
+app.post('/charge', function (request, response) {
+	// (Assuming you're using express - expressjs.com)
+	// Get the credit card details submitted by the form
+	var stripeToken = request.body.stripeToken;
+	var stripeEmail = request.body.stripeEmail;
+
+	var charge = stripe.charges.create({
+  		amount: 1000, // amount in cents, again
+  		currency: "usd",
+  		card: stripeToken,
+  		customer: stripeEmail,
+  		description: "payinguser@example.com"
+	}, function(err, charge) {
+  		if (err && err.type === 'StripeCardError') {
+    	// The card has been declined
+  		}
+	});
+	
+	// handle unexpected missing input:
+	if (!request.body || !request.body.stripeToken) return response.send({ok: false, message: 'error'});
+	chargeCard(request.body.token, response);
+	// alternatively, you could subscribeUser(request.body.token, response);
+});
+
